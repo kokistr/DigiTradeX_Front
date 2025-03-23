4e37f9cdaa76f574f4db80851e2d17e848f46c31
kokistr <125700374+kokistr@users.noreply.github.com>
2025-03-24 02:57:17 +0900

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,7 @@ module.exports = {
     '^.+\\.(js|jsx)$': 'babel-jest',
   },
   testEnvironment: 'jsdom',
+  transformIgnorePatterns: [
+    '/node_modules/(?!axios)/' // axios だけは Babel でトランスパイルする
+  ],
 };
