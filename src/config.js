92e1a035fd0ca6112997a7b5eb941adc19683c06
kokistr <125700374+kokistr@users.noreply.github.com>
2025-03-25 09:36:43 +0900

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 // src/config.js
-export const API_URL = process.env.REACT_APP_API_URL || 'https://tech0-gen-8-step4-dtx-poback-frhzgag5qubkb4gi.canadacentral-01.azurewebsites.net';
+// 環境変数から取得または固定値としてバックエンドのURLを設定
+export const API_URL = 'https://tech0-gen-8-step4-dtx-poback-frhzgag5gubkb4gj.canadacentral-01.azurewebsites.net';
