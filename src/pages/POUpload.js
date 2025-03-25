2f8465c07dc21f963585343e1bff6b241f147156
kokistr <125700374+kokistr@users.noreply.github.com>
2025-03-25 23:40:15 +0900

    Update POUpload.js

--- src/pages/POUpload.js
+++ src/pages/POUpload.js
@@ -34,7 +34,7 @@ const POUpload = () => {
     ],
 
     // Inputテーブルのフィールド（UI表示しないが保持）
-    shipment_arrangement: '手配中',
+    shipment_arrangement: '手配前',
     po_acquisition_date: new Date().toISOString().split('T')[0],
     organization: '',
     invoice_number: '',
@@ -550,7 +550,7 @@ const POUpload = () => {
       ],
 
       // Inputテーブルのフィールド - UI表示はしないが保持
-      shipment_arrangement: '手配中',
+      shipment_arrangement: '手配前',
       po_acquisition_date: new Date().toISOString().split('T')[0],
       organization: '',
       invoice_number: '',
